cb8f0377d4327cfb9d7435c9d6c93ad2671017be
Dylan <dlgastonusmc@gmail.com>
2024-04-23 11:35:05 -0500

    Solved validParenthises problem

--- src/validParenthesis.java
+++ src/validParenthesis.java
@@ -1,44 +1,46 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class validParenthesis {
-    public static boolean sequentialValid(String s, HashMap<String,String> validKeys){
-        int count =0;
-        for (int i = 0; i <s.length(); i+=2) {
-            if(validKeys.containsKey(Character.toString(s.charAt(i))) && Character.toString(s.charAt(i+1)).equals(validKeys.get(Character.toString(s.charAt(i))))){
-                count +=1;
-            }
-        }
-        return count == s.length() / 2;
-    }
-    public static boolean palindromeValid(String s, HashMap<String,String>validKeys){
-        int count=0;
-        for (int i = 0; i < s.length()/2; i++) {
-            if(validKeys.containsKey(Character.toString(s.charAt(i))) && Character.toString(s.charAt((s.length()-1)-i)).equals(validKeys.get(Character.toString(s.charAt(i))))){
-                count +=1;
-            }
-        }
-        return count == s.length() / 2;
-    }
     public static boolean isValid(String s){
+        if(s.length()<2){
+            return false;
+        }
         HashMap<String,String> validKeys = new HashMap<>();
         validKeys.put("{","}");
         validKeys.put("(",")");
         validKeys.put("[","]");
-        if(s.isEmpty()||s.length()%2!=0){
-            return false;
+        Stack<String> t = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            String digit = Character.toString(s.charAt(i));
+            if(validKeys.containsKey(digit)){
+                t.push(digit);
+            }
+            else if(!validKeys.containsKey(digit) && !t.isEmpty()){
+                String pair = t.peek();
+                if(!digit.equals(validKeys.get(pair))){
+                    return false;
+                }
+                t.pop();
+            } else {
+                return false;
+            }
         }
-        return (sequentialValid(s,validKeys)||palindromeValid(s,validKeys));
+
+        return  t.isEmpty();
     }
     public static void main(String[] args){
-    String s1 = "()";
+    String s1 = ")(){}";
     String s2 = "()[]{}{}{}()";
-    String s3 = "(]";
+    String s3 = "){";
     String s4 = "{[]}";
+    String s5 = "{{[]()}()}";
 
         System.out.println(isValid(s1));
         System.out.println(isValid(s2));
         System.out.println(isValid(s3));
         System.out.println(isValid(s4));
+        System.out.println(isValid(s5));
     }
 }
