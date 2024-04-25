2ed2ee32803c136388e8f511ed9093e2093b1f7f
Dlgaston <dlgastonusmc@gmail.com>
2024-04-25 17:34:26 -0500

    Completed mergeSortedLinkedList

--- src/MergeTwoSortedList.java
+++ src/MergeTwoSortedList.java
@@ -19,13 +19,11 @@ public class MergeTwoSortedList {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         if (l1.val < l2.val) {
-            System.out.println(l1.val);
-            System.out.println(l1.next);
+
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         }
-        System.out.println(l2.val);
-        System.out.println(l2.next);
+
         l2.next = mergeTwoLists(l1, l2.next);
         return l2;
     }
